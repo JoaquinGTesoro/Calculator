1b565ab05a2bbccd80a12fda71d8c30efd623b57
Joaquin G Tesoro <joaquingonzalez.tesoro@gmail.com>
2021-06-03 16:15:18 -0300

    decimals not more than once, rounded decimals to two, more than one calc

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ function clearEverything() {
     calculation = [];
 }
 
+function round(num) {
+    var m = Number((Math.abs(num) * 100).toPrecision(15));
+    return Math.round(m) / 100 * Math.sign(num);
+}
+
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.btn');
 const operators = document.querySelectorAll('.operator');
@@ -45,6 +50,7 @@ const clearBtn = document.getElementById('clear');
 
 let enteredNumber = null;
 let operand = null;
+let decimalClicked = false;
 let calculation = [];
 
 numbers.forEach(number => {
@@ -59,6 +65,13 @@ numbers.forEach(number => {
             if (operand != null) {
                 calculation.push(operand);
                 operand = null;
+            }
+            if (number.innerHTML == ".") {
+                if (decimalClicked == true) {
+                    return 1;
+                } else {
+                    decimalClicked = true;
+                }
             }
             display.innerHTML += number.innerHTML;
             if (enteredNumber == null) {
@@ -75,6 +88,7 @@ operators.forEach(operator => {
     operator.addEventListener('click', () => {
         if (enteredNumber != null) calculation.push(parseFloat(enteredNumber));
         enteredNumber = null;
+        decimalClicked = false;
         operand = operator.innerHTML;
         display.innerHTML = "";
     })
@@ -85,13 +99,22 @@ equalBtn.addEventListener('click', () => {
     if(operand != null) calculation.push(operand);
     enteredNumber = null;
     operand = null;
+    decimalClicked = false;
+    let result;
     if (calculation.length == 1) {
-        display.innerHTML = calculation[0]; 
+        result = calculation[0]; 
     } else if (calculation.length == 2) {
-        display.innerHTML = operate(calculation[0], calculation[0], checkOperand(calculation[1]));
-    } else {
-        display.innerHTML = operate(calculation[0], calculation[2], checkOperand(calculation[1]));
+        result = operate(calculation[0], calculation[0], checkOperand(calculation[1]));
+    } else if (calculation.length == 3) {
+        result = operate(calculation[0], calculation[2], checkOperand(calculation[1]));
+    } else if (calculation.length > 3){
+        while (calculation.length >= 3) {
+            result = operate(calculation[0], calculation[2], checkOperand(calculation[1]));
+            calculation[0] = result;
+            calculation.splice(1, 2);
+        }
     }
+    display.innerHTML = round(result);
     clearEverything();
     calculation[0] = parseFloat(display.innerHTML);
 });
